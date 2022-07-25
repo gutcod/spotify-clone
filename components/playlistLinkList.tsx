f3e08965e750572bfe636c46b4da972acdac5d9d
Andrei GUTAN <agutan@P5RO-PORT1706.local>
2022-07-25 14:05:37 +0300

    add some staff

--- components/playlistLinkList.tsx
+++ components/playlistLinkList.tsx
@@ -3,9 +3,14 @@ import NextLink from "next/link";
 
 const PlaylistLink = ({ playlist }) => {
   return (
-    <ListItem key={playlist.id} paddingX="20px" fontSize="16px">
+    <ListItem key={playlist.id} paddingX='20px' fontSize='16px'>
       <LinkBox>
-        <NextLink href="/" passHref>
+        <NextLink
+          href={{
+            pathname: "/playlist/[id]",
+            query: { id: playlist.id },
+          }}
+          passHref>
           <LinkOverlay>{playlist.name}</LinkOverlay>
         </NextLink>
       </LinkBox>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,30 +9,26 @@ const Home = ({ artists }) => {
   return (
     <GradientLayout
       roundImage
-      color="gray"
-      subtitle="profile"
+      color='gray'
+      subtitle='profile'
       title={`${user?.firstName} ${user?.lastName}`}
       description={`${user?.playlistsCount} public playlists`}
-      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
-    >
-      <Box color="white" paddingX="40px">
-        <Box marginBottom="40px">
-          <Text fontSize="2xl" fontWeight="bold">
+      image='https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0'>
+      <Box color='white' paddingX='40px'>
+        <Box marginBottom='40px'>
+          <Text fontSize='2xl' fontWeight='bold'>
             Top artist this month
           </Text>
-          <Text fontSize="md">only visible to you</Text>
+          <Text fontSize='md'>only visible to you</Text>
         </Box>
         <Flex>
-          {artists.map((artist) => (
-            <Box paddingX="10px" width="20%">
-              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
-                <Image
-                  src="https://placekitten.com/300/300"
-                  borderRadius="100%"
-                />
-                <Box marginTop="20px">
-                  <Text fontSize="large">{artist.name}</Text>
-                  <Text fontSize="x-small">Artist</Text>
+          {artists.map(artist => (
+            <Box paddingX='10px' width='20%'>
+              <Box bg='gray.900' borderRadius='4px' padding='15px' width='100%'>
+                <Image src={`https://picsum.photos/400?random=${artist.id}`} borderRadius='100%' />
+                <Box marginTop='20px'>
+                  <Text fontSize='large'>{artist.name}</Text>
+                  <Text fontSize='x-small'>Artist</Text>
                 </Box>
               </Box>
             </Box>
